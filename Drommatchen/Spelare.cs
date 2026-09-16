3a637717400a0a4d21b993846569ad16c0706c2f
lazo <lazo-hussain@hotmail.com>
2026-09-16 14:42:22 +0200

    jag la till konstruktor för Spelare

--- Drommatchen/Spelare.cs
+++ Drommatchen/Spelare.cs
@@ -1,30 +1,31 @@
 public class Spelare
 {
-    // Privata fält
     private string _namn;
     private int _nummer;
     private string _position;
 
-    // Properties — publik get, privat set
     public string Namn
     {
         get { return _namn; }
-    private set { _namn = value; }
+        private set { _namn = value; }
     }
 
     public int Nummer
     {
-        get { return _nummer = value; }
+        get { return _nummer; }
+        private set { _nummer = value; }
     }
 
     public string Position
     {
-        get { return _position = value; }
+        get { return _position; }
+        private set { _position = value; }
     }
 
-    // Konstruktor
     public Spelare(string namn, int nummer, string position)
     {
-        // TODO: tilldela de privata fälten
+        Namn = namn;
+        Nummer = nummer;
+        Position = position;
     }
 }
